33a7d7656dad90be826f959d6ac1c26c0932ad54
Raul Granados <00138816@uca.edu.sv>
2020-06-28 20:51:18 -0600

    Agregando imagenes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -23,19 +23,39 @@ public class Controller implements Initializable {
         if(current > 0){
             current-=1;
         }
-        loadResources();
+        change();
+        System.out.println(current);
     }
 
     public void siguiente(){
-        current+=1;
-        loadResources();
+        if(current < 3){
+            current+=1;
+        }
+        change();
+    }
+
+    public void change(){
+        tema.setText(titulos.get(current));
+        main_image.setImage(imagenes.get(current));
     }
 
     public void loadResources(){
         titulos.add("Dominio a utilizar");
         tema.setText(titulos.get(current));
 
+        titulos.add("Malla a utilizar");
+        tema.setText(titulos.get(current));
+
+        titulos.add("Malla a utilizar");
+        tema.setText(titulos.get(current));
+
+        titulos.add("Malla a utilizar");
+        tema.setText(titulos.get(current));
+
         imagenes.add(new Image("https://raw.githubusercontent.com/ragranados/DesafioSimu/master/src/assets/dominio.png?token=AI32UN54UXED7C32KDW3O6C7AJ576"));
+        main_image.setImage(imagenes.get(current));
+
+        imagenes.add(new Image("https://raw.githubusercontent.com/ragranados/DesafioSimu/master/src/assets/malla.png?token=AI32UN36EOYYAQ4VS3W35WC7AKAEE"));
         main_image.setImage(imagenes.get(current));
     }
 
