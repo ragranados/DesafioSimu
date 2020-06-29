8c7dc2687da16df25021b66ac3eb98c5245d698c
Raul Granados <00138816@uca.edu.sv>
2020-06-28 19:33:53 -0600

    nueva imagen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.image.Image;
@@ -10,22 +11,36 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    public Button boton_prueba;
+    public Button anterior;
+    public Button siguiente;
     public ImageView main_image;
-    //public Image image_change;
+    public Label tema;
     ArrayList<Image> imagenes = new ArrayList();
+    ArrayList<String> titulos = new ArrayList();
+    int current = 0;
 
+    public void anterior(){
+        if(current > 0){
+            current-=1;
+        }
+        loadResources();
+    }
 
-    public void loadImages(){
-        Image imagen = new Image("https://pbs.twimg.com/media/EboBtVbXkAEkDm6?format=jpg&name=small");
-        //imagenes.add(imagen);
-        main_image.setImage(imagen);
-        System.out.println("hola");
+    public void siguiente(){
+        current+=1;
+        loadResources();
+    }
+
+    public void loadResources(){
+        titulos.add("Dominio a utilizar");
+        tema.setText(titulos.get(current));
+
+        imagenes.add(new Image("https://raw.githubusercontent.com/ragranados/DesafioSimu/master/src/assets/dominio.png?token=AI32UN54UXED7C32KDW3O6C7AJ576"));
+        main_image.setImage(imagenes.get(current));
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        loadImages();
-
+        loadResources();
     }
 }
