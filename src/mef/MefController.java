fc641f34d10513db4d66fa13ede20668c8338b44
Raul Granados <00138816@uca.edu.sv>
2020-06-29 01:57:22 -0600

    Agregando paso 2

--- src/mef/MefController.java
+++ src/mef/MefController.java
@@ -1,13 +1,20 @@
 package mef;
 
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class MefController {
+public class MefController implements Initializable {
     public Button anterior;
     public Button siguiente;
     public Label tema;
@@ -31,9 +38,26 @@ public class MefController {
         change();
     }
 
+    public void regresarARecursos() {
+        Stage stage = new Stage();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/recursos/recursos.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+            stage.setTitle("Recursos");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        cerrarVentana();
+    }
+
     public void anterior() {
 
         if (current == 0) {
+            regresarARecursos();
             cerrarVentana();
         }
 
@@ -46,6 +70,23 @@ public class MefController {
     }
 
     public void change() {
+        tema.setText(titulos.get(current));
+        main_image.setImage(imagenes.get(current));
+    }
 
+    public void loadResources() {
+        //Paso 1
+        titulos.add("Paso 1");
+        tema.setText(titulos.get(current));
+
+        imagenes.add(new Image("https://raw.githubusercontent.com/ragranados/DesafioSimu/master/src/assets/paso1.png?token=AI32UN5NBL3ZIUKPSTXDNUC7ALIDO"));
+        main_image.setImage(imagenes.get(current));
+
+        explicaciones.add("");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadResources();
     }
 }
